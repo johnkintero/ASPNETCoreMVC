37e5df4132ebba03d7cd7d0fbb618b41589ee810
johnkintero <johnjairoqp@gmail.com>
2020-06-20 19:59:21 -0500

    Paso de datos del modelo a la vista y render sections

--- AlphaWebApp/Controllers/BookController.cs
+++ AlphaWebApp/Controllers/BookController.cs
@@ -23,7 +23,7 @@ namespace AlphaWebApp.Controllers
         public ViewResult GetAllBooks()
         {
             var data = _bookRepository.GetAllBooks();
-            return View();
+            return View(data);
         }
 
         public BookModel GetBook(int id)
--- AlphaWebApp/Repository/BookRepository.cs
+++ AlphaWebApp/Repository/BookRepository.cs
@@ -25,11 +25,13 @@ namespace AlphaWebApp.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id=1, Title="MVC", Author="John"},
-                new BookModel(){Id=2, Title="GIT", Author="John"},
-                new BookModel(){Id=3, Title="NETCORE", Author="Milena"},
-                new BookModel(){Id=4, Title="JAVASCRIPT", Author="Lorena"},
-                new BookModel(){Id=5, Title="PYTHON", Author="Tatiana"},
+                new BookModel(){Id=1, Title="MVC", Author="John", Description="Explica el modelo vista controlador"},
+                new BookModel(){Id=2, Title="GIT", Author="John", Description="Aprenda a manejar GIT"},
+                new BookModel(){Id=3, Title="NETCORE", Author="Milena",Description="La nueva microsoft de codigo abierto .NetCore"},
+                new BookModel(){Id=4, Title="JAVASCRIPT", Author="Lorena", Description="Secretos de Javascript"},
+                new BookModel(){Id=5, Title="PYTHON", Author="Tatiana",Description="Aprenda a domar la serpiente"},
+                new BookModel(){Id=5, Title="DISEÑO", Author="Tatiana",Description="Aprenda a diseñar su Web"},
+                new BookModel(){Id=5, Title="MECANICA", Author="Jose",Description="Mecanica para Dummis"},
             };
         }
 
